e10f7e30622801efc8d411aad00d345a244da911
takahashi kouichi <takahashi.kouichi334@mail.canon>
2020-01-16 12:21:12 +0900

    init

---
+++ RubyFull/RubyFull/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubyFull
+//
+//  Created by takahashi kouichi on 2020/01/16.
+//  Copyright © 2020 kouichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
