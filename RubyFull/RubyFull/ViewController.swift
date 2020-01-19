a0416ed3d9ab729e39f02bd1bcc291adad2a4ca2
tkou15 <tkou15.work@gmail.com>
2020-01-19 12:20:43 +0900

    #7 キーボードの高さに合わせて画面をずらす

--- RubyFull/RubyFull/ViewController.swift
+++ RubyFull/RubyFull/ViewController.swift
@@ -17,10 +17,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputTextField: AnimatableTextField!
     @IBOutlet weak var inputStringLabel: UILabel!
     @IBOutlet weak var hiraganaLabel: UILabel!
-    
     @IBOutlet weak var resultStackView: UIStackView!
     
     var alertController: UIAlertController!
+    
+    private var inputString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,48 +30,93 @@ class ViewController: UIViewController {
         self.view.backgroundColor = UIColor.mainColor
         self.resultStackView.isHidden = true
         
-        cardView.animate(.slideFade(way: .in, direction: .up), duration: 2, damping: 1, velocity: 2, force: 1)
+        cardView.animate(.slideFade(way: .in, direction: .up), duration: 1, damping: 1, velocity: 2, force: 1)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.notificationObserver()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
     @IBAction func action(_ sender: Any) {
+        // 文字列の入力を確定
+        self.view.endEditing(true)
+        self.inputString = self.inputTextField.text!
         
-        guard let inputString = self.inputTextField.text else { return }
+        // 入力された文字列のValidation
         let status = KanjiValidator.validate(inputString) { $0
             .isNotEmpty()
             .hasKanji()
         }
         
         switch status {
-        case .valid:
+        case .valid: //
             let request = HiraganaRequest(inputString: inputString)
             Session.send(request) { result in
                 switch result {
                 case .success(let response):
-                    self.displaySuccess(response)
+                    self.displaySuccess(hiragana: response.hiragana)
                 case .failure(let error):
                     print(error)
                 }
             }
-        case .invalid(.hasNotKanji):
+        case .invalid(.hasNotKanji): // 漢字を含んでいなかったら
             self.displayAlert(errorTitle: InvalidID.hasNotKanji.alertTitle)
-        case .invalid(.empty):
+        case .invalid(.empty): // 文字列が空だったら
             self.displayAlert(errorTitle: InvalidID.empty.alertTitle)
         }
     }
     
-    private func displaySuccess(_ response: HiraganaResponse){
+    // MARK: - DisplayResult
+    
+    /// 変換後のひらがなを表示する
+    /// - Parameter response: 変換後のひらがな
+    private func displaySuccess(hiragana: String){
+        // 変換後の文字列を表示する
         self.resultStackView.isHidden = false
         self.inputStringLabel.text = inputTextField.text
-        self.hiraganaLabel.text = response.hiragana
+        self.hiraganaLabel.text = hiragana
     }
     
     private func displayAlert(errorTitle: String){
-        alertController = UIAlertController(title: errorTitle,
-                                   message: nil,
-                                   preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "OK",
-                                       style: .default,
-                                       handler: nil))
+        self.resultStackView.isHidden = true
+        
+        alertController = UIAlertController(title: errorTitle, message: nil, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alertController, animated: true)
+    }
+    
+    // MARK: - Notification
+    
+    /// Notification発行
+    private func notificationObserver() {
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                 name: UIResponder.keyboardWillShowNotification, object: nil)
+        notification.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                 name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    /// キーボード表示時に画面をずらす
+    @objc func keyboardWillShow(_ notification: Notification?) {
+        guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            let transform = CGAffineTransform(translationX: 0, y: -(self.cardView.frame.height))
+            self.view.transform = transform
+        }
+    }
+    
+    /// キーボード終了時に画面を戻す
+    @objc func keyboardWillHide(_ notification: Notification?) {
+        guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            self.view.transform = CGAffineTransform.identity
+        }
     }
 }
